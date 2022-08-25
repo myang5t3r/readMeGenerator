e33574b29fb6408bcee62a19867bfbc35dec0ac2
myang5t3r <matthew.yang@hunterdouglas.com>
2022-08-24 23:19:33 -0600

    Added function to render function links

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -22,19 +22,38 @@ function renderLicenseBadge(license) {
 
 //Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch(license){
+    case 'MIT':
+      link= 'https://choosealicense.com/licenses/mit/';
+      return link;
+    case 'Apache 2.0':
+      link='https://choosealicense.com/licenses/apache-2.0/';
+      return link;
+    case 'GNU GPLv3':
+      link= 'https://choosealicense.com/licenses/gpl-2.0/';
+      return link;
+    case 'ISC':
+      link='https://choosealicense.com/licenses/isc/';
+      return link;
+    case 'None':
+      link='';
+      return link;
+  }; 
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
 
-// TODO: Create a function to generate markdown for README
+// Create a function to generate markdown for README
 function generateMarkdown(data) {
   // Use functions above to create items on markdown
   // renderLicenseBadge(data.license)
   // generate Markdown here
   let mkdwObject = `
-  # ${data.title}
+  ## ${data.title}
+  ___
   ## Table on Contents
   1. [${data.title}](#${data.title})
   2. [License](#License)
@@ -46,14 +65,21 @@ function generateMarkdown(data) {
   8. [Questions](#Questions)
   ___
   ## License
-  [![](${renderLicenseBadge(data.license)})](https://choosealicense.com/licenses/mit/)
+  [![](${renderLicenseBadge(data.license)})](${renderLicenseLink(data.license)})
+  ___
   ## Description
   ${data.projectDescription}
+  ___
   ## Installation
+  ___
   ## Usage
+  ___
   ## Contributing
+  ___
   ## Tests
+  ___
   ## Questions
+  ___
   `
   // Return markdown to it can be saved
   return mkdwObject;
